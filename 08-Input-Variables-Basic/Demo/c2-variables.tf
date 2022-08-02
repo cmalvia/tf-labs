60e0cdf9980a6ad472a241bcc6e8bae96a4ee7ed
Chetan Malvia <53286038+cmalvia@users.noreply.github.com>
2022-08-03 07:40:01 +0800

    Add files via upload

---
+++ 08-Input-Variables-Basic/Demo/c2-variables.tf
@@ -0,0 +1,25 @@
+# Input Variables
+
+# 1. Business Unit Name
+
+
+# 2. Environment Name
+
+
+# 3. Resource Group Name
+
+
+# 4. Resource Group Location
+
+# 5. Virtual Network Name
+
+
+# YOU CAN ADD LIKE THIS MANY MORE argument values from each resource
+# 6. Subnet Name
+# 7. Public IP Name
+# 8. Network Interface Name
+# 9. Virtual Machine Name
+# 10. VM OS Disk Name
+# 11. .....
+# 12. ....
+
---
+++ 13-Terraform-Local-Values/terraform-manifests/c3-local-values.tf
@@ -0,0 +1,17 @@
+# Local Values Block
+
+locals {
+  # Use-case-1: Shorten the names for more readability
+  rg_name = "${var.business_unit}-${var.environment}-${var.resoure_group_name}"
+  vnet_name = "${var.business_unit}-${var.environment}-${var.virtual_network_name}"
+
+  # Use-case-2: Common tags to be assigned to all resources
+  service_name = "Demo Services"
+  owner = "Chetan"
+  common_tags = {
+    Service = local.service_name
+    Owner = local.owner
+  }
+
+ 
+}  
